2b2968eef5d88da1fdcdc220420b7dd5537dae19
Sori Kang <56942589+Kaaaaaaaaaang@users.noreply.github.com>
2021-04-20 12:03:14 +0900

    #2 Console.WriteLine()을 써서 출력

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -20,6 +20,13 @@ namespace CSBasic
             // int break;
             var name = "강소리";
             Console.WriteLine("Hello");
+
+            // 출력
+            Console.Write("이건 안개행");
+            Console.Write("입니당\n");
+
+            Console.WriteLine("");
+            Console.WriteLine("이건 개행");
         }
     }
 }
