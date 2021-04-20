d6c2e51e4568a12b9c60dda05fff1bb345a63152
Sori Kang <56942589+Kaaaaaaaaaang@users.noreply.github.com>
2021-04-20 11:52:33 +0900

    #1 기본 용어

--- CSBasic/Program.cs
+++ CSBasic/Program.cs
@@ -8,8 +8,18 @@ namespace CSBasic
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            // keyword
+            // int as;
+            // context keyword
+            int 한글;
+            // int has space;
+            // int 263alpha;
+            // int break;
+            var name = "강소리";
+            Console.WriteLine("Hello");
         }
     }
 }
