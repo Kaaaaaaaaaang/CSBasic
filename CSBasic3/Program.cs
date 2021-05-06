dd687eeab27f9d4e3b9918ad64fdb9f9f215d403
Sori Kang <56942589+Kaaaaaaaaaang@users.noreply.github.com>
2021-05-06 10:01:40 +0900

    #20 while문 실습

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -17,6 +17,26 @@ namespace CSBasic3
             Console.WriteLine(intArray[3]);
             Console.WriteLine(intArray[100]); // 범위 초과
             Console.WriteLine(intArray[-1]); // 음수 사용 불가
+
+            int i = 0;
+            int[] intArray2 = { 52, 273, 32, 65, 103 };
+
+            // 반복을 수행한다.
+            while (i < intArray2.Length)
+            {
+                // 출력한다.
+                Console.WriteLine(i + "번째 출력 : " + intArray2[i]);
+
+                // 탈출을 위해 변수를 더한다.
+                i++;
+            }
+
+            string input;
+            do
+            {
+                Console.Write("입력(exit을 입력하면 종료) :");
+                input = Console.ReadLine();
+            } while (input != "exit");
         }
     }
 }
