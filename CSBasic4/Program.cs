c436e4975fb29e7d9b8e7e77b5fc4f00f95d149a
s2019s01 <s2019s01@e-mirim.hs.kr>
2021-05-27 08:58:53 +0900

    #24 클래스 개요, 사용, 생성

--- CSBasic4/Program.cs
+++ CSBasic4/Program.cs
@@ -6,10 +6,75 @@ using System.Threading.Tasks;
 
 namespace CSBasic4
 {
+    class Car
+    {
+        int carNumber;
+        DateTime inTime;
+        DateTime outTime;
+
+        public void SetInTime()
+        {
+            this.inTime = DateTime.Now;
+        }
+        public void SetOutTime()
+        {
+            this.outTime = DateTime.Now;
+        }
+    }
+    class FirstClass
+    {
+
+    }
+    class SecondClass
+    {
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            // Car[] cars = new Car[10];
+            Car car = new Car();
+            car.SetInTime();
+            car.SetOutTime();
+
+            Random random = new Random();
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+
+            int[] intArray = new int[10];
+            long[] longArray = new long[10];
+            string[] stringArray = new string[10];
+
+            List<int> list = new List<int>();
+
+            list.Add(52);
+            list.Add(273);
+            list.Add(32);
+            list.Add(64);
+
+            foreach(var item in list)
+            {
+                Console.WriteLine("Count : " + list.Count + "\titem : " + item);
+            }
+
+            Console.WriteLine(Math.Abs(-52273));
+            Console.WriteLine(Math.Ceiling(52.273));
+            Console.WriteLine(Math.Floor(52.273));
+            Console.WriteLine(Math.Max(52, 273));
+            Console.WriteLine(Math.Min(52, 273));
+            Console.WriteLine(Math.Round(52.273));
         }
     }
+
 }
